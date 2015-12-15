e90b464087755c1e9c487da9a923883660b20f39
evilz <evilznet@gmail.com>
2015-12-15 16:37:49 +0100

    Correct Deep First Search by two method - Browse - FindPath Add Unit Tests

--- src/Algorithms/GraphTraversal/DepthFirstSearch.cs
+++ src/Algorithms/GraphTraversal/DepthFirstSearch.cs
@@ -6,7 +6,7 @@ namespace Algorithms.GraphTraversal
 {
     public static class DepthFirstSearch
     {
-        public static IEnumerable<T> FindPath<T>(T start,T end, Func<T,IEnumerable<T>> getNeighbours)
+        public static IEnumerable<T> BrowseGraph<T>(T start, Func<T, IEnumerable<T>> getNeighbours)
         {
             var visited = new HashSet<T>();
             var stack = new Stack<T>();
@@ -17,19 +17,49 @@ namespace Algorithms.GraphTraversal
             {
                 var current = stack.Pop();
 
-                if (!visited.Add(current))
-                    continue;
+                if (!visited.Add(current)) { continue; }
 
                 yield return current;
 
                 var neighbours = getNeighbours(current)
-                                      .Where(n => !visited.Contains(n));
+                                    .Where(n => !visited.Contains(n));
 
-                foreach (var neighbour in neighbours)
+                foreach (var neighbour in neighbours.Reverse())
                     stack.Push(neighbour);
             }
-           
+
         }
 
+        public static IEnumerable<T> FindPath<T>(T start, T end, Func<T, IEnumerable<T>> getNeighbours)
+        {
+            var visited = new HashSet<T>();
+            var stack = new Stack<T>();
+            var path = new Stack<T>();
+
+            stack.Push(start);
+
+            while (stack.Any())
+            {
+                var current = stack.Pop();
+                path.Push(current);
+
+                if (!visited.Add(current)) { continue; }
+
+                if (current.Equals(end))
+                    return path.Reverse();
+
+                var neighbours = getNeighbours(current)
+                                    .Where(n => !visited.Contains(n));
+
+                if (!neighbours.Any())
+                    path.Pop();
+
+                foreach (var neighbour in neighbours.Reverse())
+                    stack.Push(neighbour);
+            }
+
+            return Enumerable.Empty<T>();
+
+        }
     }
 }
--- tests/AlgorithmTests/GraphTraversal/DepthFirstSearchTests.cs
+++ tests/AlgorithmTests/GraphTraversal/DepthFirstSearchTests.cs
@@ -27,27 +27,125 @@ namespace AlgorithmTests.GraphTraversal
 
             Func<Point, char> getCell = (p) =>
              {
-                 return maze.Split(new[] { Environment.NewLine },StringSplitOptions.None)[p.Y][p.X];
+                 return maze.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Skip(1).ToArray()[p.Y][p.X];
              };
 
             Func<Point, IEnumerable<Point>> getNeighbours = (p) =>
              {
-                 var allPoints = new Point[] {
-                new Point(p.X, p.Y - 1),
-                 new Point(p.X + 1, p.Y),
-                 new Point(p.X, p.Y + 1),
-                 new Point(p.X - 1, p.Y),
-             };
+                 var allPoints = new Point[]
+                 {
+                    new Point(p.X, p.Y - 1), // top
+                     new Point(p.X + 1, p.Y), // right
+                     new Point(p.X, p.Y + 1), // bottom
+                     new Point(p.X - 1, p.Y), // left
+                 };
 
                  return allPoints.Where(x => getCell(x) != '%');
-                 
+
              };
 
 
-            var result = DepthFirstSearch.FindPath(start, end, getNeighbours);
+            var result = DepthFirstSearch.BrowseGraph(start, getNeighbours);
 
-            var expected = new[] { Point.Empty };
-            Assert.That(result, Is.EquivalentTo(expected));
+            List<Point> path = new List<Point>();
+            foreach (var item in result)
+            {
+                path.Add(item);
+                if (item == end) break;
+            }
+
+            var expected = new[] {
+                new Point(9,3),
+                new Point(10,3),
+                new Point(10,2),
+                new Point(10,1),
+                new Point(11,1),
+                new Point(12,1),
+                new Point(13,1),
+                new Point(14,1),
+                new Point(13,2),
+                new Point(13,3),
+                new Point(14,3),
+                new Point(15,3),
+                new Point(16,3),
+                new Point(16,2),
+                new Point(16,1),
+                new Point(17,1),
+                new Point(18,1),
+                new Point(18,2),
+                new Point(18,3),
+                new Point(18,4),
+                new Point(18,5),
+                new Point(17,5),
+                new Point(16,5),
+                new Point(15,5),
+                new Point(14,5),
+                new Point(13,5),
+                new Point(12,5),
+                new Point(11,5),
+                new Point(10,5),
+                new Point(9,5),
+                new Point(8,5),
+                new Point(7,5),
+                new Point(6,5),
+                new Point(5,5),
+                new Point(4,5),
+                new Point(3,5),
+                new Point(2,5),
+                new Point(1,5),
+
+            };
+            Assert.That(path, Is.EquivalentTo(expected));
+        }
+
+        // TODO 
+        [Test]
+        public void Small_maze_FindPath_test()
+        {
+            var start = new Point(9, 3);
+            var end = new Point(1, 5);
+
+            var maze = @"
+%%%%%%%%%%%%%%%%%%%%
+%--------------%---%
+%-%%-%%-%%-%%-%%-%-%
+%--------P-------%-%
+%%%%%%%%%%%%%%%%%%-%
+%.-----------------%  
+%%%%%%%%%%%%%%%%%%%%";
+
+            Func<Point, char> getCell = (p) =>
+            {
+                return maze.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Skip(1).ToArray()[p.Y][p.X];
+            };
+
+            Func<Point, IEnumerable<Point>> getNeighbours = (p) =>
+            {
+                var allPoints = new Point[]
+                {
+                    new Point(p.X, p.Y - 1), // top
+                     new Point(p.X + 1, p.Y), // right
+                     new Point(p.X, p.Y + 1), // bottom
+                     new Point(p.X - 1, p.Y), // left
+                };
+
+                return allPoints.Where(x => getCell(x) != '%');
+
+            };
+
+
+            var result = DepthFirstSearch.FindPath(start,end, getNeighbours);
+
+            for (int i = 0; i < result.Count() - 1; i++)
+            {
+                var TwoPoints = result.Skip(i).Take(2);
+                var point1 = TwoPoints.First();
+                var point2 = TwoPoints.Last();
+                var distance = Math.Abs(point1.X - point2.X) + Math.Abs(point1.Y - point2.Y);
+                Assert.That(distance, Is.EqualTo(1));
+            }
+        
+            Assert.That(result.Last(), Is.EqualTo(end));
         }
 
 
