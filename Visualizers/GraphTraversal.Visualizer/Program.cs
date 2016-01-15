01eaa5e45d08aa0b464aaee2f06f84b60820e4c2
evilz <evilznet@gmail.com>
2016-01-15 08:53:33 +0100

    update Astar and visualizer withmore advanced maze

--- Visualizers/GraphTraversal.Visualizer/Program.cs
+++ Visualizers/GraphTraversal.Visualizer/Program.cs
@@ -11,15 +11,15 @@ namespace GraphTraversal.Visualizer
     {
         private const string MAZE =
 @"%%%%%%%%%%%%%%%%%%%%
-%--------------%---%
+%-----@--------%---%
 %-%%-%%-%%-%%-%%-%-%
-%--------S-------%-%
+%--------S-@-----%-%
 %%%%%%%%%%%%%%%%%%-%
-%------------------%  
-%------------------%
-%------------------%
-%.-----------------%
-%------------------%
+%----%%%%%%%%%%----%  
+%----%------@------%
+%----%--@@----@@---%
+%.---%-----@-------%
+%----%%%%%%%%%%@-@@%
 %------------------%
 %%%%%%%%%%%%%%%%%%%%";
 
@@ -48,22 +48,22 @@ namespace GraphTraversal.Visualizer
         {
             var start = new Point(9, 3);
             var end = new Point(1, 8);
-            var currentMaze = MAZE;
             Func<Point, char> getCell = GetCell(MAZE);
             Func<Point, IEnumerable<Point>> getNeighbours = GetNeighbours(getCell);
-            Func<Point, Point, int> getCost = (from, to) => (to.X + to.Y)%2 == 0 ? 1 : 3;
+            Func<Point, Point, int> getCost = (from, to) => getCell(to) == '@' ? 4:1 ;
             Func<Point, int> manhattanHeuristic = (to) => Math.Abs(to.X - end.X) + Math.Abs(to.Y - end.Y);
-            var millisecondsTimeout = 300;
+            var millisecondsTimeout = 100;
 
-            var algorithms = new Dictionary<int, Tuple<string, Func<IEnumerable<Point>>, Func<IEnumerable<Point>>>> 
+            var algorithms = new Dictionary<int, Tuple<string, Func<IEnumerable<Point>>, Func<IEnumerable<Point>>>>
             {
                 {1, "Depth First Search", () =>  DepthFirstSearch.Explore(start, getNeighbours),()=>DepthFirstSearch.FindPath(start,getNeighbours,p => p.Equals(end) ) },
                 {2, "Breadth First Search", () =>  BreadthFirstSearch.Explore(start, getNeighbours) ,()=>BreadthFirstSearch.FindPath(start,getNeighbours,p => p.Equals(end) )},
                 {3, "Dijkstra", () =>  Dijkstra.Explore(start, getNeighbours,getCost ) ,()=>Dijkstra.FindPath(start,getNeighbours,getCost,p => p.Equals(end) )},
                 {4, "Greedy Best-First Search (with manhattan)", () => GreedyBestFirstSearch.Explore(start, getNeighbours, manhattanHeuristic),()=>GreedyBestFirstSearch.FindPath(start,getNeighbours,manhattanHeuristic,p => p.Equals(end) )},
+                {5, "A* (with manhattan)", () => AStar.Explore(start, getNeighbours,getCost, manhattanHeuristic),()=>AStar.FindPath(start,getNeighbours,getCost,manhattanHeuristic,p => p.Equals(end) )},
             };
 
-            
+
             int choice = 0;
             while (choice != 10)
             {
@@ -77,49 +77,53 @@ namespace GraphTraversal.Visualizer
 
 
                 while (choice == 0)
-            {
-                int.TryParse(Console.ReadLine(), out choice);
-            }
-            
-            var result = algorithms[choice].Item2();
-            var path = algorithms[choice].Item3().ToList();
+                {
+                    int.TryParse(Console.ReadLine(), out choice);
+                }
+                var currentMaze = MAZE;
 
-            var visited = new List<Point>();
-            
-            foreach (var item in result)
-            {
+                var result = algorithms[choice].Item2();
+                var path = algorithms[choice].Item3().ToList();
 
-                visited.Add(item);
-                var lines = currentMaze.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
-                var line = lines[item.Y].ToCharArray();
-                line[item.X] = '*';
-                lines[item.Y] = new string(line);
-                currentMaze = string.Join(Environment.NewLine, lines);
-                DisplayMaze(currentMaze, visited.Count,0,algorithms[choice].Item1);
-                
-                Thread.Sleep(millisecondsTimeout);
-                if (item == end) break;
+                var visited = new List<Point>();
 
-            }
-            
-            foreach (var item in path)
-            {
-                var lines = currentMaze.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-                var line = lines[item.Y].ToCharArray();
-                line[item.X] = '+';
-                lines[item.Y] = new string(line);
-                currentMaze = string.Join(Environment.NewLine, lines);
-            }
-            DisplayMaze(currentMaze, visited.Count(),path.Count(), algorithms[choice].Item1);
-            
-            choice = 0;
-            Console.WriteLine();
-            Console.WriteLine($"Press any key to reset");
-            Console.Read();
+                foreach (var item in result)
+                {
+
+                    visited.Add(item);
+                    var lines = currentMaze.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    var line = lines[item.Y].ToCharArray();
+                    line[item.X] = '*';
+                    lines[item.Y] = new string(line);
+                    currentMaze = string.Join(Environment.NewLine, lines);
+                    DisplayMaze(currentMaze, visited.Count, 0,0, algorithms[choice].Item1);
+
+                    Thread.Sleep(millisecondsTimeout);
+                    if (item == end) break;
+
+                }
+
+                foreach (var item in path)
+                {
+                    var lines = currentMaze.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    var line = lines[item.Y].ToCharArray();
+                    line[item.X] = '+';
+                    lines[item.Y] = new string(line);
+                    currentMaze = string.Join(Environment.NewLine, lines);
+                }
+
+                var pathcost = path.Aggregate(0, (current, point) => current + getCost(Point.Empty, point));
+
+                DisplayMaze(currentMaze, visited.Count(), path.Count(),pathcost, algorithms[choice].Item1);
+
+                choice = 0;
+                Console.WriteLine();
+                Console.WriteLine($"Press any key to reset");
+                Console.Read();
             }
         }
 
-        private static void DisplayMaze(string currentMaze,int visitedCount,int pathCount,string algoName )
+        private static void DisplayMaze(string currentMaze, int visitedCount, int pathCount,int pathCost, string algoName)
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
@@ -127,17 +131,20 @@ namespace GraphTraversal.Visualizer
             Console.ResetColor();
             Console.WriteLine($"Explored node count : {visitedCount}");
             Console.WriteLine($"Path length : {pathCount}");
+            Console.WriteLine($"Path cost : {pathCount}");
             Console.WriteLine();
             foreach (var c in currentMaze.ToList())
             {
-                if(c == '*')
+                if (c == '*')
                     Console.BackgroundColor = ConsoleColor.DarkRed;
-                if(c == '+')
+                if (c == '+')
                     Console.ForegroundColor = ConsoleColor.Green;
+
+                
                 Console.Write(c);
                 Console.ResetColor();
             }
-            
+
         }
     }
 }
---
+++ src/Algorithms/GraphTraversal/AStar.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// F = G + H
+namespace Algorithms.GraphTraversal
+{
+    public class AStar
+    {
+
+        class PriorityQueue<TPriority, TItem> : IEnumerable<KeyValuePair<TPriority, TItem>>
+        {
+            readonly SortedDictionary<TPriority, Queue<TItem>> _subqueues;
+
+            public PriorityQueue()
+            {
+                _subqueues = new SortedDictionary<TPriority, Queue<TItem>>(Comparer<TPriority>.Default);
+            }
+
+            public void Enqueue(TPriority priority, TItem item)
+            {
+                if (!_subqueues.ContainsKey(priority))
+                {
+                    _subqueues.Add(priority, new Queue<TItem>());
+                }
+
+                _subqueues[priority].Enqueue(item);
+            }
+
+            public KeyValuePair<TPriority, TItem> Dequeue()
+            {
+                if (_subqueues.Any())
+                {
+                    var first = _subqueues.First();
+                    var nextItem = first.Value.Dequeue();
+                    if (!first.Value.Any())
+                    {
+                        _subqueues.Remove(first.Key);
+                    }
+                    return new KeyValuePair<TPriority, TItem>(first.Key, nextItem);
+                }
+                else
+                    throw new InvalidOperationException("The queue is empty");
+            }
+
+            public IEnumerator<KeyValuePair<TPriority, TItem>> GetEnumerator()
+            {
+                var q = _subqueues.SelectMany(pair => pair.Value.Select(item => new KeyValuePair<TPriority, TItem>(pair.Key, item)));
+                return q.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)_subqueues).GetEnumerator();
+            }
+        }
+
+        public static IEnumerable<T> Explore<T>(T start, Func<T, IEnumerable<T>> getNeighbours, Func<T, T, int> getCost, Func<T, int> heuristic, Func<T, bool> isEnd = null)
+        {
+            return astar(start, getNeighbours, getCost, heuristic, false, isEnd);
+        }
+
+        private static IEnumerable<T> astar<T>(T start, Func<T, IEnumerable<T>> getNeighbours, Func<T, T, int> getCost, Func<T, int> heuristic, bool pathOnly,
+            Func<T, bool> isEnd = null)
+        {
+            var visitedFromAndCost = new Dictionary<T, Tuple<T, int>>();
+            var toVisit = new PriorityQueue<int, T>();
+
+            toVisit.Enqueue(0, start);
+            visitedFromAndCost.Add(start, new Tuple<T, int>(default(T), 0));
+
+            while (toVisit.Any())
+            {
+                var currentNode = toVisit.Dequeue();
+                var current = currentNode.Value;
+                var currentCost = currentNode.Key;
+
+
+                if (!pathOnly)
+                {
+                    yield return current;
+                }
+
+                if (isEnd != null && isEnd(current))
+                {
+                    if (pathOnly)
+                    {
+                        var path = new Queue<T>();
+
+                        while (!current.Equals(start))
+                        {
+                            path.Enqueue(current);
+                            current = visitedFromAndCost[current].Item1;
+                        }
+                        path.Enqueue(current);
+                        while (path.Any())
+                        {
+                            yield return path.Dequeue();
+                        }
+
+                    }
+                    yield break;
+                }
+
+                var neighboursWithCost = getNeighbours(current)
+                    .Select(n => new { Neighbour = n, NewCost = visitedFromAndCost[current].Item2 + getCost(current, n) })
+                    .Where(x =>
+                        !visitedFromAndCost.ContainsKey(x.Neighbour)
+                        || x.NewCost < visitedFromAndCost[x.Neighbour].Item2)
+                    .ToList();
+
+                foreach (var x in neighboursWithCost)
+                {
+                    var priority = x.NewCost + heuristic(x.Neighbour);
+                    toVisit.Enqueue(priority, x.Neighbour);
+                    visitedFromAndCost[x.Neighbour] = new Tuple<T, int>(current, x.NewCost);
+                }
+            }
+
+        }
+
+        public static IEnumerable<T> FindPath<T>(T start, Func<T, IEnumerable<T>> getNeighbours, Func<T, T, int> getCost, Func<T, int> heuristic, Func<T, bool> isEnd = null)
+        {
+            return astar(start, getNeighbours, getCost, heuristic, true, isEnd);
+        }
+    }
+}
